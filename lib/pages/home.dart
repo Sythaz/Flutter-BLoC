524d471fd234020043a9f80dee3728bb6b0c0664
Sythaz <syafiqaldiansyah@gmail.com>
2024-01-20 16:41:57 +0700

    tahap blocBuilder

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:udemy_bloc/bloc/counter.dart';
 
 class HomePage extends StatelessWidget {
@@ -13,23 +14,38 @@ class HomePage extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              //StreamBuilder digunakan sebagai widget yang bisa mem-build ulang jika terdapat perubahan value pada data stream:
-              StreamBuilder(
-                //Stream berguna untuk menandai dan mengalirkan data mana yang perlu diwaspadai akan perubahannya
-                stream: mycounter.stream,
-                //InitialData seperti namanya berguna untuk menentukan value awal, namun disini hanya perlu memanggilnya karena initialData sudah di inisialisasi dalam constructor CounterCubit
-                initialData: mycounter.initialData,
-                //Snapshot berguna sebagai informasi tentang ketersediaan data (.hasData), ketersediaan error (.hasError), data terkini (.data), status koneksi (.connectionState)
-                builder: (BuildContext context, AsyncSnapshot snapshot) {
-                  return Column(
-                    children: [
-                      Text("${snapshot.data}"),
-                      Text("Current: ${mycounter.current}"),
-                      Text("Next: ${mycounter.next}")
-                    ],
-                  );
+              //BlocBuilder digunakan sebagai widget yang bisa mem-build ulang ketika terdapat perubahan dari bloc:, bedanya blocBuilder lebih optimal untuk merender UI yang terkait dan berkaitan dengan BLoC
+              BlocBuilder<CounterCubit, int>(
+                bloc: mycounter,
+                //blocBuilder memiliki buildWhen yang berguna untuk menentukan kapan builder me-rebuild UI dengan if return true dan false, previous dan current seperti namanya adalah sebuah data lama dan data saat ini
+                buildWhen: (previous, current) {
+                  if (previous == 1) {
+                    return false;
+                  } else
+                    return true;
+                },
+                //blocBuilder memiliki state, berbeda dengan streamBuilder yang memiliki snapshot yang memiliki informasi status dan data. blocBuilder state hanya memiliki informasi data saja
+                builder: (context, state) {
+                  return Text("$state");
                 },
               ),
+              // //StreamBuilder digunakan sebagai widget yang bisa mem-build ulang jika terdapat perubahan value pada data stream:
+              // StreamBuilder(
+              //   //Stream berguna untuk menandai dan mengalirkan data mana yang perlu diwaspadai akan perubahannya
+              //   stream: mycounter.stream,
+              //   //InitialData seperti namanya berguna untuk menentukan value awal, namun disini hanya perlu memanggilnya karena initialData sudah di inisialisasi dalam constructor CounterCubit
+              //   initialData: mycounter.initialData,
+              //   //Snapshot berguna sebagai informasi tentang ketersediaan data (.hasData), ketersediaan error (.hasError), data terkini (.data), status koneksi (.connectionState)
+              //   builder: (BuildContext context, AsyncSnapshot snapshot) {
+              //     return Column(
+              //       children: [
+              //         Text("${snapshot.data}"),
+              //         Text("Current: ${mycounter.current}"),
+              //         Text("Next: ${mycounter.next}")
+              //       ],
+              //     );
+              //   },
+              // ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
