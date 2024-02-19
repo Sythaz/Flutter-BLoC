6a572709afbd36176e95bbb7d1aafa40bd9d32d4
Sythaz <syafiqaldiansyah@gmail.com>
2024-02-19 20:11:51 +0700

    ganti UI counter

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -14,51 +14,93 @@ class HomePage extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              //BlocConsumer adalah gabungan antara blocListener dengan blocBuilder yang terdapat bloc: sebagai target pengawasan terhadap perubahan, buildWhen:, builder:, listener:, dan listenWhen:
-              BlocConsumer<CounterCubit, int>(
-                bloc: mycounter,
-                //blocBuilder memiliki buildWhen yang berguna untuk menentukan kapan builder me-rebuild UI dengan if return true dan false, previous dan current seperti namanya adalah sebuah data lama dan data saat ini
-                buildWhen: (previous, current) {
-                  if (previous == 1) {
-                    return false;
-                  } else
-                    return true;
-                },
-                //blocBuilder memiliki state, berbeda dengan streamBuilder yang memiliki snapshot yang memiliki informasi status dan data. blocBuilder state hanya memiliki informasi data saja
-                builder: (context, state) {
-                  return Text("$state");
-                },
-                //Function utama blocListener, bagian untuk menampilkan kebutuhan seperti dialog atau snackBar
-                listener: (context, state) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(
-                      duration: Duration(milliseconds: 200),
-                      content: Text("Genap!"),
-                    ),
-                  );
-                },
-                //Function utama blocListener, bagian untuk menampilkan kebutuhan seperti dialog atau snackBar
-                listenWhen: (previous, current) {
-                  if (current % 2 == 0) {
-                    return true;
-                  } else {
-                    return false;
-                  }
-                },
-              ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  IconButton(
-                      onPressed: () {
-                        mycounter.kurangData();
-                      },
-                      icon: Icon(Icons.remove)),
-                  IconButton(
-                      onPressed: () {
+                  Material(
+                    borderRadius: BorderRadius.circular(15),
+                    color: Colors.blue,
+                    child: InkWell(
+                        borderRadius: BorderRadius.circular(20),
+                        onTap: () {
+                          mycounter.kurangData();
+                        },
+                        child: SizedBox(
+                          height: 80,
+                          width: 50,
+                          child: Icon(
+                            Icons.remove,
+                            color: Colors.white,
+                          ),
+                        )),
+                  ),
+
+                  //BlocConsumer adalah gabungan antara blocListener dengan blocBuilder yang terdapat bloc: sebagai target pengawasan terhadap perubahan, buildWhen:, builder:, listener:, dan listenWhen:
+                  BlocConsumer<CounterCubit, int>(
+                    bloc: mycounter,
+                    //blocBuilder memiliki buildWhen yang berguna untuk menentukan kapan builder me-rebuild UI dengan if return true dan false, previous dan current seperti namanya adalah sebuah data lama dan data saat ini
+                    buildWhen: (previous, current) {
+                      if (previous == 1) {
+                        return false;
+                      } else
+                        return true;
+                    },
+                    //blocBuilder memiliki state, berbeda dengan streamBuilder yang memiliki snapshot yang memiliki informasi status dan data. blocBuilder state hanya memiliki informasi data saja
+                    builder: (context, state) {
+                      return Material(
+                          borderRadius: BorderRadius.circular(20),
+                          color: Colors.red,
+                          child: SizedBox(
+                            width: 160,
+                            height: 80,
+                            child: Center(
+                              child: Text(
+                                "$state",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 45),
+                              ),
+                            ),
+                          ));
+                    },
+                    //Function utama blocListener, bagian untuk menampilkan kebutuhan seperti dialog atau snackBar
+                    listener: (context, state) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          duration: Duration(milliseconds: 200),
+                          content: Text("Genap!"),
+                        ),
+                      );
+                    },
+                    //Function utama blocListener, bagian untuk menampilkan kebutuhan seperti dialog atau snackBar
+                    listenWhen: (previous, current) {
+                      if (current % 2 == 0) {
+                        return true;
+                      } else {
+                        return false;
+                      }
+                    },
+                  ),
+
+                  Material(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.blue,
+                    child: InkWell(
+                      borderRadius: BorderRadius.circular(20),
+                      child: SizedBox(
+                        height: 80,
+                        width: 50,
+                        child: Icon(
+                          Icons.add,
+                          color: Colors.white,
+                        ),
+                      ),
+                      onTap: () {
                         mycounter.tambahData();
                       },
-                      icon: Icon(Icons.add))
+                    ),
+                  )
                 ],
               ),
             ],
